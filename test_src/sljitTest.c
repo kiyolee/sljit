f0fb728027fa59ec773edae292cd709a347265bd
Kelvin Lee <kiyolee@gmail.com>
2020-08-17 23:29:36 +1000

    Disable test64 when using libsljit.dll.

--- test_src/sljitTest.c
+++ test_src/sljitTest.c
@@ -5474,6 +5474,7 @@ static void test50(void)
 
 static void test51(void)
 {
+#ifndef SLJIT_CONFIG_DLL
 	/* Test mov addr with absolute label addresses */
 	executable_code code;
 	sljit_uw malloc_addr;
@@ -5581,6 +5582,10 @@ static void test51(void)
 	FAILED(buf[8] != offs5, "test51 case 10 failed\n");
 
 	sljit_free_code(code.code, NULL);
+#else
+	if (verbose)
+		printf("Skip test51\n");
+#endif
 
 	successful_tests++;
 }
